32adbd6a2e6ae5b1e0b4ca0cbb6c72cda3c4a4fd
fmoraless <franticzero@gmail.com>
2023-04-01 14:36:24 -0300

    trying Prop Types

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Curso React Native</Text>
-      <Saludar />
-      {/* <Saludar firstname="Francisco" lastname="Morales" /> */}
+      {/* <Saludar /> */}
+      <Saludar firstname="Francisco" lastname={28} />
       <Saludar firstname="Carlos" lastname="Navarro" />
     </View>
   );
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,5 +1,6 @@
 import react from "react";
 import { Text } from "react-native";
+import PropTypes from "prop-types";
 
 export default function Saludar(props) {
     const { firstname, lastname } = props;
@@ -11,7 +12,12 @@ export default function Saludar(props) {
     );
 }
 
-Saludar.defaultProps = {
+/* Saludar.defaultProps = {
     firstname: "Francisco", 
     lastname:  "Morales"
-}
+} */
+
+Saludar.propTypes = {
+    firstname: PropTypes.string.isRequired,
+    lastname: PropTypes.string
+};
